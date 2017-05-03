4e86ec5bad6b715a16ef09973af2567755933607
veivi <juhani.vehvilainen@fimnet.fi>
2017-05-03 11:25:56 +0300

    Cleanup (stuff moved to vpStatus record)

--- libraries/AP_Compass/examples/Paska/Status.cpp
+++ libraries/AP_Compass/examples/Paska/Status.cpp
@@ -4,16 +4,3 @@
 //
 //
 
-bool pitotFailed()
-{
-  return vpStatus.fault == 1
-    || (!vpStatus.simulatorLink && pitotDevice.status());
-}
-
-bool alphaFailed()
-{
-  return vpStatus.alphaLocked
-    || vpStatus.fault == 2
-    || (!vpStatus.simulatorLink && alphaDevice.status());
-}
-
