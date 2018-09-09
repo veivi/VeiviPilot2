836dc37a29bf81faacc0662bbbf813ccfab19bfe
veivi <juhani.vehvilainen@fimnet.fi>
2018-09-09 21:35:58 +0300

    YD tweaked (quicker washout)

--- libraries/AP_Compass/examples/Paska/MainLoop.c
+++ libraries/AP_Compass/examples/Paska/MainLoop.c
@@ -103,12 +103,12 @@ void mainLoopSetup()
   slopeInit(&trimRateLimiter, 3/RADIAN);
   slopeInit(&rollAccelLimiter, 0);
   
-  damperInit(&ball, 1.5*CONTROL_HZ, 0);
+  damperInit(&ball, 0.5*CONTROL_HZ, 0);
   damperInit(&iasFilter, 2, 0);
   damperInit(&iasFilterSlow, 3*CONTROL_HZ, 0);
   damperInit(&accAvg, 2*CONTROL_HZ, G);
 
-  washoutInit(&yawDamper, 0.5*CONTROL_HZ, 0);
+  washoutInit(&yawDamper, 0.3*CONTROL_HZ, 0);
   
   swAvgInit(&liftFilter, CONFIG_HZ/4);
 
