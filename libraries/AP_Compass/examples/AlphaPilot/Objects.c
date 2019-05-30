938fb305262c24f587335ff0b7f6b1a5186c36bb
veivi <juhani.vehvilainen@fimnet.fi>
2019-05-30 14:48:19 +0300

    Cosmetic tweaks (Objects.c)

--- libraries/AP_Compass/examples/AlphaPilot/Objects.c
+++ libraries/AP_Compass/examples/AlphaPilot/Objects.c
@@ -12,11 +12,17 @@ struct OutputState vpOutput;
 // struct GPSFix gpsFix;
 
 float controlCycle;
-float logBandWidth, ppmFreq, simInputFreq;
+float logBandWidth, ppmFreq;
 uint32_t idleMicros;
 uint8_t gaugeCount, gaugeVariable[MAX_PARAMS];
 uint32_t lastPPMWarn;
 float fieldStrength;
+
+struct SimLinkSensor sensorData;
+uint16_t simFrames;
+float simInputFreq;
+int linkDownCount, heartBeatCount;
+uint32_t simTimeStamp;
 
 PIDCtrl_t elevCtrl = PIDCTRL_CONS(1);
 PIDCtrl_t pushCtrl = PIDCTRL_CONS(1);
@@ -24,7 +30,8 @@ PIDCtrl_t rudderCtrl = PIDCTRL_U_CONS(RATIO(1/3));
 PIDCtrl_t aileCtrl = PIDCTRL_U_CONS(RATIO(2/3));
 Damper_t avgDynP = DAMPER_CONS(8*CONFIG_HZ, 0);
 Damper_t accAvg = DAMPER_CONS(4*CONFIG_HZ, 9.81);
-Sampler_t alphaSampler = SAMPLER_CONS, iasSampler = SAMPLER_CONS;
+Sampler_t alphaSampler = SAMPLER_CONS;
+Sampler_t iasSampler = SAMPLER_CONS;
 SlopeLimiter_t aileActuator = SLOPE_CONS(0);
 SlopeLimiter_t trimRateLimiter = SLOPE_CONS(3/RADIAN);
 SlopeLimiter_t flapActuator = SLOPE_CONS(0.5);
@@ -33,7 +40,3 @@ Turbine_t engine;
 SWAvg_t liftFilter = SWAVG_CONS(CONFIG_HZ/4);
 SWAvg_t primaryIASDataFilter = SWAVG_CONS(CONTROL_HZ/8);
 
-struct SimLinkSensor sensorData;
-uint16_t simFrames;
-int linkDownCount, heartBeatCount;
-uint32_t simTimeStamp;
