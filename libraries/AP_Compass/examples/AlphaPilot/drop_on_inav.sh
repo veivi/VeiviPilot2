9a2660b9f3e7a6beb3da03e2768cd54763df07ea
Juhani Vehvilainen <juhani.vehvilainen@fimnet.fi>
2020-11-16 16:14:04 +0200

    Tweaks continue

--- libraries/AP_Compass/examples/AlphaPilot/drop_on_inav.sh
+++ libraries/AP_Compass/examples/AlphaPilot/drop_on_inav.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-if [ ! -x src/main/AlphaPilot ]
+target=$1
+
+if [ -z "$target" ]
 then
-    mkdir -p src/main/AlphaPilot/TARGET/INAV
+    echo "Target specifier missing."
+    exit 0
+fi
+
+if [ ! -x $ALPHAPILOT/AlphaPilot/TARGET/INAV/$target ]
+then
+    echo "Target INAV/$target not found."
+    exit 0
+fi
+
+if [ ! -x src/main/AlphaPilot/TARGET/$target ]
+then
+    mkdir -p src/main/AlphaPilot/TARGET/$target
 fi
 
 update="$ALPHAPILOT/AlphaPilot/update_file.bash"
 
 $update "$ALPHAPILOT/AlphaPilot/*.h" src/main/AlphaPilot
 $update "$ALPHAPILOT/AlphaPilot/*.c" src/main/AlphaPilot
-$update "$ALPHAPILOT/AlphaPilot/TARGET/INAV/*.h" src/main/AlphaPilot/TARGET/INAV
-$update "$ALPHAPILOT/AlphaPilot/TARGET/INAV/*.c" src/main/AlphaPilot/TARGET/INAV
+$update $ALPHAPILOT/AlphaPilot/TARGET/INAV/$target/StaP_TARGET.h src/main/AlphaPilot/TARGET/$target
+$update $ALPHAPILOT/AlphaPilot/TARGET/INAV/$target/StaP.c src/main/AlphaPilot/TARGET/$target
 $update $ALPHAPILOT/AlphaPilot/TARGET/INAV/main.c src/main
 $update $ALPHAPILOT/AlphaPilot/TARGET/INAV/CMakeLists.txt src/main
 $update $ALPHAPILOT/AlphaPilot/TARGET/INAV/main.cmake cmake
+
+# $update $ALPHAPILOT/AlphaPilot/TARGET/INAV/bus_i2c.h src/main/drivers
+# $update $ALPHAPILOT/AlphaPilot/TARGET/INAV/bus_i2c_impl.h src/main/drivers
+# $update $ALPHAPILOT/AlphaPilot/TARGET/INAV/$target/bus_i2c_stm32f10x.c src/main/drivers
+
+
 
